039ab2dee928f7f434b44c15ec0e3a18eae7f0a6
Shashi Kiran G M <shashi.kiran@raifox.com>
2018-12-26 19:41:35 +0530

    Linked Application Description page

--- src/app/ui/application-info-page/application-info-page.component.ts
+++ src/app/ui/application-info-page/application-info-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-application-info-page',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./application-info-page.component.scss']
 })
 export class ApplicationInfoPageComponent implements OnInit {
+  private app_type: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.app_type = params['app'];
+    });
   }
 
+  ngOnInit() {
+    if (this.app_type != null && this.app_type == "smaps") {
+      $("#application_title").text("Shaastra Maps");
+      $("#app_desc_first_para").text("Shaastra Maps is an advanced feature in Vedavaapi, it connects words present in multiple books and verses.");
+      $("#app_desc_second_para").css("display", "none");
+    }
+  }
+  
 }
