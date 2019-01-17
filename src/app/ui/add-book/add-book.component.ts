694200bb4434cfd524d4146d0cdeb1139c4d5f38
Shashi Kiran G M <shashi.kiran@raifox.com>
2019-01-17 15:28:42 +0530

    Internet explorer errors fixed; Add book works in all the three browsers: Firefox, Chrome and Internet Explorer

--- src/app/ui/add-book/add-book.component.ts
+++ src/app/ui/add-book/add-book.component.ts
@@ -197,8 +197,8 @@ export class AddBookComponent implements OnInit {
 
     const uploadData = new FormData();
     uploadData.append('files', this.selectedPageFile);
-    uploadData.set("files_purpose", "pagination");
-    uploadData.set("resource_json", JSON.stringify(page_json));
+    uploadData.append("files_purpose", "pagination");
+    uploadData.append("resource_json", JSON.stringify(page_json));
 
 
     this.spinner.show();
@@ -402,7 +402,7 @@ export class AddBookComponent implements OnInit {
           } else if (errorCode == 400) {
             $("#error-add-book").text("Bad Request. Please contact the platform administartor.");
           } else {
-            $("#error-add-book").text("Unknown erorr, please contact the platform administrator or retry after some time.");
+            $("#error-add-book").text("Unknown error, please contact the platform administrator or retry after some time.");
           }
 
           $("#error-add-book").css("display", "block");
@@ -510,7 +510,7 @@ export class AddBookComponent implements OnInit {
 
     const uploadData = new FormData();
     uploadData.append('files', this.selectedThumbnailFile);
-    uploadData.set("files_purpose", "thumbnail");
+    uploadData.append("files_purpose", "thumbnail");
 
     this.http.post(this.endpointService.getBaseUrl() + "/ullekhanam/v1/resources/"
       + this.uploadedBookId + "/files", uploadData, httpUploadOptions)
