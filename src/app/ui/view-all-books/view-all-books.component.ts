17444518d6996f2cf56bda3e17c6487f2aa273b7
Shashi Kiran G M <shashi.kiran@raifox.com>
2019-01-29 16:55:03 +0530

    declared all the server endpoint properties in endpoints.services.ts file. Performed some minor code cleanup.

--- src/app/ui/view-all-books/view-all-books.component.ts
+++ src/app/ui/view-all-books/view-all-books.component.ts
@@ -11,16 +11,13 @@ import * as $ from 'jquery';
 export class ViewAllBooksComponent implements OnInit {
 
   private bookDetails : any;
-  private imgFileIds :number[] = [];
-  private bookNames : string[] = [];
-  private image_id: any;
-  public showCount : number= 0;
+  private showCount : number= 0;
+  //stores book images, title.
+  private bookStoreData = [];
   
   constructor(private endpointService: EndpointsService, private http: HttpClient) {
 
   }
-  // store book images, title.
-  bookStoreData = [];
   
  /**
    * POST request to set the working repository.
@@ -31,19 +28,22 @@ export class ViewAllBooksComponent implements OnInit {
   setRepository() {
     const httpUploadOptions = { headers: new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'}), 
                         withCredentials: true };
-    return this.http.post(this.endpointService.getBaseUrl() + '/store/v1/repos', 'repo_name=demo',
+    return this.http.post(this.endpointService.getBaseUrl() + '/store/v1/repos', 'repo_name=' + this.endpointService.getRepositoryName(),
      httpUploadOptions);
   }
    
  
 //  passing the book image and title to template
-  addBookDetails(index,imgFileIds,bookNames){
+  addBookDetails(imgFileIds,bookNames){
+    var iif_image_url = null;
+
     if(imgFileIds !=null){
-      this.image_id="https://api.vedavaapi.org/py/iiif_image/v1/demo/ullekhanam/"+imgFileIds+"/full/100,150/0/default.jpg";
+      iif_image_url = this.endpointService.getBaseUrl() + "/iiif_image/v1/" + this.endpointService.getRepositoryName()
+                      + "/ullekhanam/"+imgFileIds+"/full/100,150/0/default.jpg";
     }else{
-      this.image_id="assets/default-book-īmg.png";   
+      iif_image_url ="assets/default-book-īmg.png";   
     }
-       this.bookStoreData.push({Name:bookNames,img:this.image_id});
+    this.bookStoreData.push({Name:bookNames,img:iif_image_url});
   }
 
   /**
@@ -51,34 +51,36 @@ export class ViewAllBooksComponent implements OnInit {
    *
   */
   public loadMore(){
-      this.bookNames = [];
-      this.imgFileIds = [];
       $('#load-more-btn-wrapper').css('display','none');
       $('#loadding-gif-icon').show();
+
     this.setRepository().subscribe(
-      data => {
-        let httpImageParams = new HttpParams().set('selector_doc','{"jsonClass": "BookPortion"}')
-         .set('associated_resources','{"files": {"purpose":"thumbnail"}}')
-         .set('start',this.showCount.toString()).set('numbers','24');
+      success => {
+        let httpImageParams = new HttpParams().append('selector_doc','{"jsonClass": "BookPortion"}')
+         .append('associated_resources','{"files": {"purpose":"thumbnail"}}')
+         .append('start',this.showCount.toString())
+         .append('numbers','24');
          this.showCount = this.showCount + 24;
+
         let httpImageHeaders : HttpHeaders = new HttpHeaders({
           'Content-Type':'application/json',
         });  
         
         this.http.get(this.endpointService.getBaseUrl() + '/ullekhanam/v1/resources' , {headers:httpImageHeaders,params:httpImageParams,withCredentials: true,})
         .subscribe(          
-          Response =>{
-            this.bookDetails = Response;  
+          response =>{
+            this.bookDetails = response;  
             if(this.bookDetails.length != 0 ){ 
               $('#loadding-gif-icon').css('display','none');
               for(let i=0;i<this.bookDetails.length;i++){
-                this.imgFileIds.push(this.bookDetails[i]["associated_resources"]["files"][0]);
-                this.bookNames.push(this.bookDetails[i]["title"]["chars"]); 
-              }
-              for(let index in this.bookNames){             
-                this.addBookDetails(index,this.imgFileIds[index],this.bookNames[index]);
-              }   
-            }
+                if(this.bookDetails[i].hasOwnProperty("associated_resources") &&  this.bookDetails[i].hasOwnProperty("title")
+                  && this.bookDetails[i]["associated_resources"].hasOwnProperty("files") 
+                  &&  this.bookDetails[i]["title"].hasOwnProperty("chars")){
+                    this.addBookDetails(this.bookDetails[i]["associated_resources"]["files"][0],this.bookDetails[i]["title"]["chars"]);
+                }
+              }//for 
+            }//if
+
             if(this.bookDetails.length >= 24){
               $('#load-more-btn-wrapper').show();
             }else{             
@@ -86,9 +88,11 @@ export class ViewAllBooksComponent implements OnInit {
             } 
           }
         );
+      },
+      error => {
+        //TODO: Show error 
       }
     );
-      // this.showCount = this.showCount+24;
   }
  
 
@@ -98,10 +102,11 @@ export class ViewAllBooksComponent implements OnInit {
     $('#loadding-gif-icon').show();
   //  get book details
     this.setRepository().subscribe(
-      data => {
-        let httpImageParams = new HttpParams().set('selector_doc','{"jsonClass": "BookPortion"}')
-          .set('associated_resources','{"files": {"purpose":"thumbnail"}}')
-          .set('start',this.showCount.toString()).set('numbers','24');
+      success => {
+        let httpImageParams = new HttpParams().append('selector_doc','{"jsonClass": "BookPortion"}')
+          .append('associated_resources','{"files": {"purpose":"thumbnail"}}')
+          .append('start',this.showCount.toString())
+          .append('numbers','24');
           this.showCount = this.showCount + 24;
         let httpImageHeaders : HttpHeaders = new HttpHeaders({
           'Content-Type':'application/json',
@@ -109,30 +114,36 @@ export class ViewAllBooksComponent implements OnInit {
         
         this.http.get(this.endpointService.getBaseUrl() + '/ullekhanam/v1/resources' , {headers:httpImageHeaders,params:httpImageParams,withCredentials: true,})
         .subscribe(          
-          Response =>{
-            this.bookDetails = Response;              
+          response =>{
+            this.bookDetails = response;              
             $('#loadding-gif-icon').css('display','none');
-            if(this.bookDetails.length != 0){
+
+            if(this.bookDetails != null && this.bookDetails.length != 0){
               $('#load-more-btn-wrapper').show(); 
               for(let i=0;i<this.bookDetails.length;i++){
-                this.imgFileIds.push(this.bookDetails[i]["associated_resources"]["files"][0]);
-                this.bookNames.push(this.bookDetails[i]["title"]["chars"]); 
-                if(this.bookDetails[i]["title"]["chars"] ==null){
-                  $('#load-more-btn-wrapper').css('display','none');
+                
+                if(this.bookDetails[i].hasOwnProperty("associated_resources") &&  this.bookDetails[i].hasOwnProperty("title")
+                    && this.bookDetails[i]["associated_resources"].hasOwnProperty("files") 
+                    &&  this.bookDetails[i]["title"].hasOwnProperty("chars")){
+
+                    if(this.bookDetails[i]["title"]["chars"] ==null){
+                      $('#load-more-btn-wrapper').css('display','none');
+                    }else{
+                      this.addBookDetails(this.bookDetails[i]["associated_resources"]["files"][0],this.bookDetails[i]["title"]["chars"]);
+                    }
                 }              
               }
-              for(let index in this.bookNames){             
-                this.addBookDetails(index,this.imgFileIds[index],this.bookNames[index]);
-              }     
             } else{
-              
               $('#load-more-btn-wrapper').css('display','none');
-            }
-          }
+            } //if-else
+          }//response callback
         );
+      },
+      error => {
+        //TODO: Display error message
       }
     );
   }
 
-}
+}//end of class declaration
 
